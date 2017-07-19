a23220a6c7fa8de1acd22f0481bbb0c8bb2f31da
Philip Peitsch <philipp@questps.com.au>
2017-07-19 16:56:52 +1000

    Reduce minimum .NET standard to 1.3
    Makes the non-DependencyContext overload extension method only visible to .NET framework, as this must have a non-null value in order for
    configuration to work correctly.
    
    Fixes #60

--- src/Serilog.Settings.Configuration/ConfigurationLoggerConfigurationExtensions.cs
+++ src/Serilog.Settings.Configuration/ConfigurationLoggerConfigurationExtensions.cs
@@ -45,26 +45,41 @@ namespace Serilog
             return settingConfiguration.ConfigurationSection(configuration.GetSection(DefaultSectionName), dependencyContext);
         }
 
+#if !NETSTANDARD1_3
         /// <summary>
         /// Reads logger settings from the provided configuration section.
         /// </summary>
         /// <param name="settingConfiguration">Logger setting configuration.</param>
         /// <param name="configuration">The Serilog configuration section</param>
-        /// <param name="dependencyContext">The dependency context from which sink/enricher packages can be located. If not supplied, the platform
-        /// default will be used.</param>
+        /// <returns>An object allowing configuration to continue.</returns>
+        public static LoggerConfiguration ConfigurationSection(
+            this LoggerSettingsConfiguration settingConfiguration,
+            IConfigurationSection configuration)
+        {
+            return ConfigurationSection(
+                    settingConfiguration,
+                    configuration,
+                    DependencyContext.Default);
+        }
+#endif
+
+        /// <summary>
+        /// Reads logger settings from the provided configuration section.
+        /// </summary>
+        /// <param name="settingConfiguration">Logger setting configuration.</param>
+        /// <param name="configuration">The Serilog configuration section</param>
+        /// <param name="dependencyContext">The dependency context from which sink/enricher packages can be located.</param>
         /// <returns>An object allowing configuration to continue.</returns>
         public static LoggerConfiguration ConfigurationSection(
             this LoggerSettingsConfiguration settingConfiguration,
             IConfigurationSection configuration,
-            DependencyContext dependencyContext = null)
+            DependencyContext dependencyContext)
         {
             if (settingConfiguration == null) throw new ArgumentNullException(nameof(settingConfiguration));
             if (configuration == null) throw new ArgumentNullException(nameof(configuration));
-            
-            return settingConfiguration.Settings(
-                new ConfigurationReader(
-                    configuration,
-                    dependencyContext ?? (Assembly.GetEntryAssembly() != null ? DependencyContext.Default : null)));
+
+            return settingConfiguration.Settings(new ConfigurationReader(configuration, dependencyContext));
         }
+
     }
 }
